8f8c8f539f7d0b516f2b5bf638cbc7d3af3f119f
marcoos127 <marcosviniciuspds@outlook.com>
2019-06-18 16:54:30 -0300

    Adicionando FAVICON

--- agendamento.php
+++ agendamento.php
@@ -8,7 +8,7 @@
   <meta name="author" content="">
 
   <title>Luketa's Enterprise - Imóveis</title>
-
+  <link rel="icon" type="imagem/png" href="img/favicon.png" />
   <!-- Bootstrap core CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
